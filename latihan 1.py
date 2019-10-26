fee6dc6658256c70f1f91032535ed8ae11cd73a1
halimbp <56398829+halimbp@users.noreply.github.com>
2019-10-26 11:52:32 +0700

    Add files via upload

---
+++ latihan 1.py
@@ -0,0 +1,11 @@
+#ini Variabel & Nilai
+
+namalengkap = "Halim budi prawira"
+namapangggilan = "Halim"
+npm = "311910729"
+tempatlahir = "Jakarta"
+umur = "18"
+tlf = "0878 7636 5646"
+alamat = "Cibarusah"
+
+print ("I was born in ",tempatlahir,"and I am",umur,"years old. I am very glad if you want to invite my house in",alamat,". So, don't forget to call me before with the number",tlf,"\n thank you")
